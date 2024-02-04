c363d9bc1bc9ecf5bbcf5e80d06dca8370a0c34f
HangaWilfried <wilfriedhanga5@gmail.com>
2024-02-04 14:45:34 +0100

    implement login page

---
+++ src/components/TwButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button :class="['font-bold flex justify-center items-center rounded-lg', theme.toLowerCase()]">
+    <slot name="content" :cta="cta">{{ cta }}</slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  cta: string;
+  theme: string;
+}>()
+</script>
+
+<style scoped>
+.blue {
+  @apply bg-blue-500 text-white px-2 py-4
+}
+</style>
